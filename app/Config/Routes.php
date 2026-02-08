a9297123da65f1b5c2a8fb02a7d7f004150ddca0
YusufDurmaz <ydurmaz2021@gmail.com>
2026-02-08 17:07:43 +0300

    locale destekli routelar oluşturuldu

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->group("{locale}/panel", static function ($routes) {
+    $routes->get("/", "Home::index", [
+        "as" => "home",
+        "filter" => "permission:dashboard.view",
+    ]);
+});
+
+$routes->group("{locale}/panel", static function ($routes) {
+    service("auth")->routes($routes);
+});
