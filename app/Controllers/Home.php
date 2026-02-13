e12914ae3f73e0a79d876d298411671a275cf7f6
YusufDurmaz <ydurmaz2021@gmail.com>
2026-02-14 00:14:12 +0300

    denemeeee

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,10 +7,10 @@ class Home extends BaseController
     public function index(): string
     {
         $configModel = new \App\Models\AppConfigModel();
-        $title = lang("App.dashboard");
+        $title = lang("App.dashboardd");
 
         $data = [
-            "title" => $title,
+            "titleDeneme" => $title,
         ];
 
         return view("dashboard/index", $data);
